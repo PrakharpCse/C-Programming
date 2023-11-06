beb5baaf245d44b181051cf107da0635904c4e88
Prakhar Piyush <prakhar.piyush@stu.adamasuniversity.ac.in>
2023-11-06 18:49:54 +0530

    print all numbers using arrays

---
+++ arrays.2.c
@@ -0,0 +1,8 @@
+//print all numbers using arrays
+#include<stdio.h>
+int main() {
+    int numbers[4] = {201, 210, 301, 324};
+    for (int i = 0; i < 4; i++) {
+        printf("%d\n", numbers[i]);
+    }
+}
